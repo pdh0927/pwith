51adf7648cc3b5b6634a1ada3521944f79fba2d4
dh <pdh980927@naver.com>
2024-10-02 18:23:36 +0900

    create project

---
+++ android/app/src/main/kotlin/com/example/pwith/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pwith
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  runApp(const ProviderScope(child: _App()));
+}
+
+class _App extends StatelessWidget {
+  const _App();
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
+    );
+  }
+}
