cdcb851b6f5bb86672b6638b238837ae6485a220
dh <pdh980927@naver.com>
2024-11-14 14:29:11 +0900

    feat: create week challenge

--- lib/common/view/home_screen.dart
+++ lib/common/view/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pwith/common/component/recent_plogging.dart';
 import 'package:pwith/common/component/total_data.dart';
+import 'package:pwith/common/component/week_challenge.dart';
 import 'package:pwith/common/layout/default_layout.dart';
 import 'package:pwith/plogging/shared_preferences/shared_preferences.dart';
 
@@ -11,19 +12,23 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return DefaultLayout(
       child: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 10),
-            RecentPlogging(),
-            SizedBox(height: 20),
-            TotalData(),
-            ElevatedButton(
-                onPressed: () async {
-                  await removePloggingData();
-                },
-                child: Text('지우기'))
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 10),
+              const RecentPlogging(),
+              const SizedBox(height: 20),
+              const TotalData(),
+              const SizedBox(height: 20),
+              const WeekChallenge(),
+              ElevatedButton(
+                  onPressed: () async {
+                    await removePloggingData();
+                  },
+                  child: Text('지우기'))
+            ],
+          ),
         ),
       ),
     );
